7546ba53b457fccdb2d680a7f52a2b4ee90236d4
rdubowski <rafaldud@gmail.com>
2021-03-29 12:06:52 +0200

    Change allowed hosts third time

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://your-book.herokuapp.com', 'https://your-book.herokuapp.com/#/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'your-book.herokuapp.com', 'your-book.herokuapp.com/#/']
 
 # Application definition
 INSTALLED_APPS = [
